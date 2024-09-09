85499d58e2ba7e8692258d294248d2a2ca8c05c8
FrederikJM <141719608+FrederikJM@users.noreply.github.com>
2024-09-09 16:24:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from .rules import doorRule
 
 model = ifcopenshell.open("path/to/ifcfile.ifc")
 
-windowResult = windowRule.checkRule(model)
-doorResult = doorRule.checkRule(model)
+storeys_in_model=len(model.by_type('IfcBuildingStorey'))
 
-print("Window result:", windowResult)
-print("Door result:", doorResult)
+if storeys_in_model == 1:
+    print(f"\nThere is {storeys_in_model} storey in the model.")
+else:
+    print(f"\nThere are {storeys_in_model} storeys in the model.")
+
+
+#windowResult = windowRule.checkRule(model)
+#doorResult = doorRule.checkRule(model)
+
+#print("Window result:", windowResult)
+#print("Door result:", doorResult)
