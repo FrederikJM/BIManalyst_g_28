3f47e19996f346f672d4d8a6a59bdad6cf34dba8
user0zero <delriama0@gmail.com>
2024-09-16 16:26:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import ifcopenshell
 
 from .rules import windowRule
 from .rules import doorRule
+#from .rules import Number_of_Storeys
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+model = ifcopenshell.open("C:\\Users\\User\\Desktop\\BIM 2024\\Assignment 1\\CES_BLD_24_06_STR.ifc")
 
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
-Number_of_storeysResult = Number_of_storeys.checkRule(model)
+#Number_of_storeysResult = Number_of_storeys.checkRule(model)
 
 print("Window result:", windowResult)
 print("Door result:", doorResult)
-print("Storeys results:", Number_of_storeysResult)
+#print("Storeys results:", Number_of_storeysResult)
