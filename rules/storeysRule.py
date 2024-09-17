c5bf424d5e4af1f6c83b40c2b1731fd39791f839
FrederikJM <141719608+FrederikJM@users.noreply.github.com>
2024-09-17 12:59:14 +0200

    d

--- rules/storeysRule.py
+++ rules/storeysRule.py
@@ -3,14 +3,10 @@ import ifcopenshell #Import IFCOpenShell
 def checkRule(model):
     storeys_in_model=len(model.by_type('IfcBuildingStorey'))
 
-    # result = f"Storeys: {storeys_in_model}"
-    
-    # return result
-
     if storeys_in_model == 1:
-        result = print(f"\nThere is {storeys_in_model} storey in the model.")
+        result = f"There is {storeys_in_model} storey in the model."
     else:
-        result = print(f"\nThere are {storeys_in_model} storeys in the model.")
+        result = f"There are {storeys_in_model} storeys in the model."
     
     return result
 
