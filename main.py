a389b40971c08508c7a29d1f068f19af890c5f1f
FrederikJM <141719608+FrederikJM@users.noreply.github.com>
2024-09-17 12:50:06 +0200

    vc

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import ifcopenshell
 
-from .rules import windowRule
-from .rules import doorRule
-#from .rules import Number_of_Storeys
+from rules import windowRule
+from rules import doorRule
+from rules import storeysRule
+
+
 
 #model = ifcopenshell.open("C:\\Users\\User\\Desktop\\BIM 2024\\Assignment 1\\CES_BLD_24_06_STR.ifc")
+#base_dir = r'/Users/frederik/Desktop/CES_BLD_24_06_ARC.ifc'
 
-model = ifcopenshell.open("/Users/frederik/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/Dokumenter/Kandidat bygningsdesign/4. semester/Advanced BIM")
+
+path_to_model = r'/Users/frederik/Desktop/CES_BLD_24_06_STR.ifc'
 
 
+model = ifcopenshell.open(path_to_model)
 
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
-#Number_of_storeysResult = Number_of_storeys.checkRule(model)
+Number_of_storeysResult = Number_of_storeys.checkRule(model)
 
 print("Window result:", windowResult)
 print("Door result:", doorResult)
-#print("Storeys results:", Number_of_storeysResult)
+print("Storeys results:", Number_of_storeysResult)
