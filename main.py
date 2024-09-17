6e08013b33e6e19ff9b48dc05b9c1e35f192961c
FrederikJM <141719608+FrederikJM@users.noreply.github.com>
2024-09-17 12:56:11 +0200

    s

--- main.py
+++ main.py
@@ -4,20 +4,13 @@ from rules import windowRule
 from rules import doorRule
 from rules import storeysRule
 
-
-
-#model = ifcopenshell.open("C:\\Users\\User\\Desktop\\BIM 2024\\Assignment 1\\CES_BLD_24_06_STR.ifc")
-#base_dir = r'/Users/frederik/Desktop/CES_BLD_24_06_ARC.ifc'
-
-
 path_to_model = r'/Users/frederik/Desktop/CES_BLD_24_06_STR.ifc'
-
 
 model = ifcopenshell.open(path_to_model)
 
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
-Number_of_storeysResult = Number_of_storeys.checkRule(model)
+Number_of_storeysResult = storeysRule.checkRule(model)
 
 print("Window result:", windowResult)
 print("Door result:", doorResult)
--- rules/storeysRule.py
+++ rules/storeysRule.py
@@ -3,16 +3,16 @@ import ifcopenshell #Import IFCOpenShell
 def checkRule(model):
     storeys_in_model=len(model.by_type('IfcBuildingStorey'))
 
-    result = storeys_in_model
-
-    return result
-
-    # if storeys_in_model == 1:
-    #     result = print(f"\nThere is {storeys_in_model} storey in the model.")
-    # else:
-    #     result = print(f"\nThere are {storeys_in_model} storeys in the model.")
+    # result = f"Storeys: {storeys_in_model}"
     
     # return result
+
+    if storeys_in_model == 1:
+        result = print(f"\nThere is {storeys_in_model} storey in the model.")
+    else:
+        result = print(f"\nThere are {storeys_in_model} storeys in the model.")
+    
+    return result
 
 
 
