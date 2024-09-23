dab1f1f3c5e8a368f56085ff8fb8edc17ce66376
FrederikJM <141719608+FrederikJM@users.noreply.github.com>
2024-09-23 16:19:34 +0200

    update new function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import ifcopenshell
 from rules import windowRule
 from rules import doorRule
 from rules import storeysRule
+from rules import slabheightRule
 
 path_to_model = r'/Users/frederik/Desktop/CES_BLD_24_06_STR.ifc'
 
@@ -11,7 +12,9 @@ model = ifcopenshell.open(path_to_model)
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
 Number_of_storeysResult = storeysRule.checkRule(model)
+SlabHeightResult = slabheightRule.checkRule(model)
 
 print("Window result:", windowResult)
 print("Door result:", doorResult)
 print("Storeys results:", Number_of_storeysResult)
+print("Slab height results:", SlabHeightResult)
